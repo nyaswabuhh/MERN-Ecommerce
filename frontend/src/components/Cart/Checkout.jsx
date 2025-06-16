5f4646559e78cafff4cbe29bfeaba4b8b8ed16df
nyaswabuhh <alex.navitech@gmail.com>
2025-06-16 22:27:41 +0300

    ppal

--- frontend/src/components/Cart/Checkout.jsx
+++ frontend/src/components/Cart/Checkout.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
+import PayPalButton from "./PayPalButton";
 
 const cart = {
   products: [
@@ -24,6 +25,7 @@ const cart = {
 
 const Checkout = () => {
   const navigate = useNavigate();
+  const [checkoutId, setCheckoutId] = useState(null);
   const [shippingAddress, setShippingAddress] = useState({
     firstName: "",
     lastName: "",
@@ -34,12 +36,21 @@ const Checkout = () => {
     phone: "",
   });
 
+  const handleCreateCheckout = (e) => {
+    e.preventDefault();
+    setCheckoutId(123);
+  };
+
+  const handlePaymentSuccess = (details) => {
+    console.log("Payment Successful", details);
+    navigate("/order-confirmation");
+  };
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 max-w-7xl mx-auto py-10 px-6 tracking-tighter">
       {/* left section */}
       <div className="bg-white  rounded-lg p-6">
         <h2 className="text-2xl uppercase mb-6">Checkout</h2>
-        <form>
+        <form onSubmit={handleCreateCheckout}>
           <h3 className="text-lg mb-4">Contact Details</h3>
           <div className="mb-4">
             <label className="block text-gray-700"></label>
@@ -75,7 +86,7 @@ const Checkout = () => {
                 onChange={(e) =>
                   setShippingAddress({
                     ...shippingAddress,
-                    lastNameName: e.target.value,
+                    lastName: e.target.value,
                   })
                 }
                 className="w-full p-2 border rounded "
@@ -159,6 +170,26 @@ const Checkout = () => {
               className="w-full p-2 border rounded"
               required
             />
+          </div>
+          <div className="mt-6">
+            {!checkoutId ? (
+              <button
+                type="submit"
+                className="w-full bg-black text-white py-3 rounded"
+              >
+                Continue to Payment
+              </button>
+            ) : (
+              <div>
+                <h3 className="text-lg mb-4">Pay with PayPal</h3>
+                {/* paypal component */}
+                <PayPalButton
+                  amount={100}
+                  onSuccess={handlePaymentSuccess}
+                  onError={(err) => alert("Payment failed, try again")}
+                />
+              </div>
+            )}
           </div>
         </form>
       </div>
