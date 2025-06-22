d2cc5fd650ae04676893643022dbcbfe92a5d051
nyaswabuhh <alex.navitech@gmail.com>
2025-06-22 19:12:27 +0300

    fix

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -61,4 +61,52 @@ router.post("/register", async (req, res) => {
   }
 });
 
+// @route POS /api/users/login
+// @desc Authenticate user
+// @access  Public
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // find the user by email
+    let user = await User.findOne({ email });
+
+    if (!user) return res.status(400).json({ message: "Invalid credentials" });
+
+    const isMatch = await user.matchPassword(password);
+
+    if (!isMatch)
+      return res.status(400).json({ message: "Invalid credentials" });
+
+    // Create JWT Payload
+    const payload = { user: { id: user._id, role: user.role } };
+
+    // sign and send token with data
+
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      { expiresIn: "40h" },
+      (err, token) => {
+        if (err) throw err;
+
+        // send user and token in response
+        res.json({
+          user: {
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+          },
+          token,
+        });
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Server error");
+  }
+});
+
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,10 @@ const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
 app.use(cors());
+
 
 dotenv.config();
 
