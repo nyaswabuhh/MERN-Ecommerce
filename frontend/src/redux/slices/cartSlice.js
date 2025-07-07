89656282cfc4bd74180265ead903be33dbdc6493
nyaswabuhh <alex.navitech@gmail.com>
2025-07-07 19:54:00 +0300

    cart redux

--- frontend/src/redux/slices/cartSlice.js
+++ frontend/src/redux/slices/cartSlice.js
@@ -32,3 +32,98 @@ export const fetchCart = createAsyncThunk(
     }
   }
 );
+
+//add an item to the cart for a user/guest
+
+export const addToCart = createAsyncThunk(
+  "cart/addToCart",
+  async (
+    { productId, quantity, size, color, guestId, userId },
+    { rejectWithValue }
+  ) => {
+    try {
+      const response = await axios.post(
+        `${import.meta.env.VITE_BACKEND_URL}/api/cart`,
+        { productId, quantity, size, color, guestId, userId }
+      );
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+
+//update quantity of an item in the cart
+
+export const updateCartItemQuantity = createAsyncThunk(
+  "cart/updateCartItemQuantity",
+  async (
+    { productId, quantity, size, color, guestId, userId },
+    { rejectWithValue }
+  ) => {
+    try {
+      const response = await axios.put(
+        `${import.meta.env.VITE_BACKEND_URL}/api/cart`,
+        { productId, color, size, quantity, guestId, userId }
+      );
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+
+//remove an item from cart
+
+export const removeFromCart = createAsyncThunk(
+  "cart/removeFromCart",
+  async ({ productId, guestId, userId, size, color }, { rejectWithValue }) => {
+    try {
+      const response = await axios({
+        method: "DELETE",
+        url: `${import.meta.env.VITE_BACKEND_URL}/api/cart`,
+        data: { productId, guestId, color, userId, size },
+      });
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+
+//merge guest cart to user cart
+
+export const mergeCart = createAsyncThunk(
+  "cart/mergeCart",
+  async ({ guestId, user }, { rejectWithValue }) => {
+    try {
+      const response = await axios.post(
+        `${import.meta.env.VITE_BACKEND_URL}/api/cart/merge`,
+        { guestId, user },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("userToken")}`,
+          },
+        }
+      );
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    cart: loadCartFromLocalStorage(),
+    loading: false,
+    error: null,
+  },
+  reducers: {
+    clearCart: (state) => {
+      state.cart = { products: [] };
+      localStorage.removeItem("cart");
+    },
+  },
+});
